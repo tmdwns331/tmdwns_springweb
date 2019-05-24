5fa758c3bdc2c015af191ded84e14cd6f9970063
tmdwns <https://github.com/tmdwns331/tmdwns-jdbc.git>
2019-05-24 16:50:37 +0900

    중간보고

--- src/main/java/org/tmdwns/article/ArticleController.java
+++ src/main/java/org/tmdwns/article/ArticleController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.tmdwns.book.chap11.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,12 +14,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 public class ArticleController {
 
 	@Autowired
 	ArticleDao articleDao;
+
+	Logger logger = LogManager.getLogger();
 
 	/**
 	 * 글 목록
@@ -52,29 +57,18 @@ public class ArticleController {
 	 */
 	@GetMapping("/article/addForm")
 	public String articleAddForm(HttpSession session) {
-		Object memberObj = session.getAttribute("MEMBER");
-		if (memberObj == null)
-			// 세션에 MEMBER가 없을 경우 로그인 화면으로
-			return "login/loginForm";
-
-return "article/addForm";
+		return "article/addForm";
 	}
 
 	/**
 	 * 글 등록
 	 */
 	@PostMapping("/article/add")
-	public String articleAdd(Article article, HttpSession session) {
-		// 세션에 MEMBER가 없을 경우 로그인 화면으로
-		Object memberObj = session.getAttribute("MEMBER");
-		if (memberObj == null)
-			// 세션에 MEMBER가 없을 경우 로그인 화면으로
-			return "login/loginForm";
-
-		Member member = (Member) memberObj;
+	public String articleAdd(Article article,
+			@SessionAttribute("MEMBER") Member member) {
 		article.setUserId(member.getMemberId());
 		article.setName(member.getName());
 		articleDao.addArticle(article);
-return "redirect:/app/article/list";
+		return "redirect:/app/article/list";
 	}
 }
